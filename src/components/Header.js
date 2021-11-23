58ee0f63b3966b3a4eddb41cdc0a6f2de74ad868
Python The Hack <lrulvoni@gmail.com>
2021-11-23 15:40:11 +0100

    second update icons

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import classes from './Header.module.scss';
+
+
+
+const Header = () => {
+    return 
+    <header className={classes.header}>   
+    <div className={classes.header__content}>
+
+        <h2>navbar</h2>
+    </div>
+
+    <nav className={classes.header__content__nav}>
+        <ul>
+            <li>
+                <a href="/">PageOne</a>
+            </li>
+            <li>
+                <a href="/">PageOne</a>
+            </li>
+            <li>
+                <a href="/">PageOne</a>
+            </li>
+
+        </ul>
+        <button>CTA PAGE</button>
+    </nav>
+    <div className={classes.header__content__togle}>
+
+
+    </div>
+    </header>;
+};
+
+export default Header;
--- src/components/Utenti.js
+++ src/components/Utenti.js
@@ -4,15 +4,50 @@ import logo1 from "../imgs/logo1.png"
 
 function Utenti() {
 
+    function Esempio() {
+        //dichiaro un valore
+        const [contatore, setContatore] = useState(0);
+        const [nome, setNome] = useState();
+        
+
+        return (
+            <>
+            <div className="contatore">
+                <p>Hai clicca {contatore} volte</p>
+                <button onClick={() => setContatore(contatore + 1)}>Clicca qui</button>
+            </div>
+            <div className="utenti">
+                <p>Tu sei {setNome}</p>
+            </div>
+            </>
+        )
+    }
+
+    function Counter({initialCount}) {
+        const [count, setCount] = useState(initialCount);
+        return (
+            <>
+            Count: {count}
+            <button onClick={() => setCount(initialCount)}>Reset</button>
+            <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+            </>
+        );
+    }
+    
+    function StatusContatore(props){
+        const [contatore, setContatore] = useState(0);
+        useEffect(() => {
+            document.title = 'Hai cliccato $(contatore) volte';
+        });
+    }
    
     return (
         <div className="welcome">
             <img src={logo1} className="welcome-logo" alt="logo" />
-            <h1>Benvenuto
-
-
-
-            </h1>
+        
+            <h1>Benvenuto</h1>
+            <h2>{Esempio()}</h2>
         </div>
     )
 }
