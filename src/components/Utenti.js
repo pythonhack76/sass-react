2ba8aac90c5c6f0470c8fc53f401134ab9130eaa
Python The Hack <lrulvoni@gmail.com>
2021-11-23 11:31:55 +0100

    nuovo livello

---
+++ src/components/Utenti.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import "./utenti.css"
+import logo1 from "../imgs/logo1.png"
+
+function Utenti() {
+
+   
+    return (
+        <div className="welcome">
+            <img src={logo1} className="welcome-logo" alt="logo" />
+            <h1>Benvenuto
+
+
+
+            </h1>
+        </div>
+    )
+}
+
+export default Utenti;
